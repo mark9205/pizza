431479dcfdbb0d50f9eea352d3a220062fd354d5
MARAT <mark9205@mail.ru>
2022-09-19 15:45:46 +0300

    + useEffect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import Categories from './components/Categories';
-import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Sort from './components/Sort';
-import './scss/app.scss';
-import pizzass from './assets/pizzas.json';
+import { useEffect, useState } from "react";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import PizzaBlock from "./components/PizzaBlock";
+import Sort from "./components/Sort";
+import "./scss/app.scss";
 
 function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {pizzass.map((pizza) => (
-              <PizzaBlock
-                types={pizza.types}
-                image={pizza.imageUrl}
-                key={pizza.id}
-                sizes={pizza.sizes}
-                title={pizza.title}
-                price={pizza.price}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const [pizzass, setPizzas] = useState([]);
+
+	useEffect(() => {
+		fetch("https://6321861e82f8687273b37ba3.mockapi.io/pizzas")
+			.then((res) => res.json())
+			.then((json) => setPizzas(json));
+	}, []);
+
+	return (
+		<div className="wrapper">
+			<Header />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Categories />
+						<Sort />
+					</div>
+					<h2 className="content__title">Все пиццы</h2>
+					<div className="content__items">
+						{pizzass.map((pizza) => (
+							<PizzaBlock
+								types={pizza.types}
+								image={pizza.imageUrl}
+								key={pizza.id}
+								sizes={pizza.sizes}
+								title={pizza.title}
+								price={pizza.price}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
