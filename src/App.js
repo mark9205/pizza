756327a1cb1f258e4cfe4800808d17f1700148e4
family <family@MacBook-Air-family.local>
2022-09-25 12:23:57 +0300

    + adaptive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
-import "./scss/app.scss";
-import { Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+import './scss/app.scss';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-	return (
-		<div className="wrapper">
-			<Header />
-			<div className="content">
-				<div className="container">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/cart" element={<Cart />} />
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,45 @@
-import React, { useEffect, useState } from "react";
-import Categories from "../components/Categories";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/PizzaBlockSkeleton";
-import Sort from "../components/Sort";
+import React, { useEffect, useState } from 'react';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlockSkeleton';
+import Sort from '../components/Sort';
 
 const Home = () => {
-	const [pizzass, setPizzas] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+  const [pizzass, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		fetch("https://6321861e82f8687273b37ba3.mockapi.io/pizzas")
-			.then((res) => res.json())
-			.then((json) => setPizzas(json))
-			.catch((err) => alert(err))
-			.finally(() => setIsLoading(false));
-	}, []);
+  useEffect(() => {
+    fetch('https://6321861e82f8687273b37ba3.mockapi.io/pizzas')
+      .then((res) => res.json())
+      .then((json) => setPizzas(json))
+      .catch((err) => alert(err))
+      .finally(() => setIsLoading(false));
+    window.scrollTo(0, 0);
+  }, []);
 
-	return (
-		<>
-			<div className="content__top">
-				<Categories />
-				<Sort />
-			</div>
-			<h2 className="content__title">Все пиццы</h2>
-			<div className="content__items">
-				{isLoading
-					? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-					: pizzass.map((pizza) => (
-							<PizzaBlock
-								types={pizza.types}
-								image={pizza.imageUrl}
-								key={pizza.id}
-								sizes={pizza.sizes}
-								title={pizza.title}
-								price={pizza.price}
-							/>
-					  ))}
-			</div>
-		</>
-	);
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+          : pizzass.map((pizza) => (
+              <PizzaBlock
+                types={pizza.types}
+                image={pizza.imageUrl}
+                key={pizza.id}
+                sizes={pizza.sizes}
+                title={pizza.title}
+                price={pizza.price}
+              />
+            ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
