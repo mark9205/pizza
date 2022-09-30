8d8dacd9fbb735c5751ab4a46da1d2708893ab43
MARAT <mark9205@mail.ru>
2022-09-30 14:54:36 +0300

    + search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import Header from './components/Header';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import './scss/app.scss';
-import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import "./scss/app.scss";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
-  );
+	const [searchValue, setSearchValue] = useState("");
+
+	return (
+		<div className="wrapper">
+			<Header searchValue={searchValue} setSearchValue={setSearchValue} />
+			<div className="content">
+				<Routes>
+					<Route path="/" element={<Home searchValue={searchValue} />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,34 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/img/pizza-logo.svg';
+import { Link } from "react-router-dom";
+import logo from "../assets/img/pizza-logo.svg";
+import Search from "./Search";
 
-function Header() {
-  return (
-    <div className="header">
-      <div className="container">
-        <Link to="/">
-          <div className="header__logo">
-            <img width="38" src={logo} alt="Pizza logo" />
-            <div>
-              <h1>React Pizza</h1>
-              <p>самая вкусная пицца во вселенной</p>
-            </div>
-          </div>
-        </Link>
-        <div className="header__cart">
-          <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
-            <div className="button__delimiter"></div>
-            <span>3</span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+function Header({ searchValue, setSearchValue }) {
+	return (
+		<div className="header">
+			<div className="container">
+				<Link to="/">
+					<div className="header__logo">
+						<img width="38" src={logo} alt="Pizza logo" />
+						<div>
+							<h1>React Pizza</h1>
+							<p>самая вкусная пицца во вселенной</p>
+						</div>
+					</div>
+				</Link>
+				<Search
+					searchValue={searchValue}
+					setSearchValue={setSearchValue}
+				/>
+				<div className="header__cart">
+					<Link to="/cart" className="button button--cart">
+						<span>520 ₽</span>
+						<div className="button__delimiter"></div>
+						<span>3</span>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
