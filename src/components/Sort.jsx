6e9cafec61bcbc7399f886ec652d125b85ae1836
family <family@MacBook-Air-family.local>
2022-10-26 20:22:49 +0300

    + hide/show popup-sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,54 +1,68 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setSort } from "../redux/slices/filterSlice";
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSort } from '../redux/slices/filterSlice';
 
 export const sortList = [
-	{ name: "по популярности ↓", sortProperty: "rating" },
-	{ name: "по популярности ↑", sortProperty: "-rating" },
-	{ name: "по цене ↓", sortProperty: "price" },
-	{ name: "по цене ↑", sortProperty: "-price" },
-	{ name: "по алфавиту ↓", sortProperty: "title" },
-	{ name: "по алфавиту ↑", sortProperty: "-title" },
+  { name: 'по популярности ↓', sortProperty: 'rating' },
+  { name: 'по популярности ↑', sortProperty: '-rating' },
+  { name: 'по цене ↓', sortProperty: 'price' },
+  { name: 'по цене ↑', sortProperty: '-price' },
+  { name: 'по алфавиту ↓', sortProperty: 'title' },
+  { name: 'по алфавиту ↑', sortProperty: '-title' },
 ];
 
 function Sort() {
-	const selected = useSelector((state) => state.filter.sort);
-	const dispatch = useDispatch();
+  const selected = useSelector((state) => state.filter.sort);
+  const dispatch = useDispatch();
+  const sortRef = useRef();
 
-	const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
+  const onClickSelected = (item) => {
+    dispatch(setSort(item));
+    setIsVisible(false);
+  };
 
-	const onClickSelected = (item) => {
-		dispatch(setSort(item));
-		setIsVisible(false);
-	};
+  useEffect(() => {
+    const clickOutside = (event) => {
+      if (!event.path.includes(sortRef.current)) {
+        setIsVisible(false);
+      }
+    };
+    document.body.addEventListener('click', clickOutside);
+    return () => {
+      document.body.removeEventListener('click', clickOutside);
+    };
+  }, []);
 
-	return (
-		<div className="sort">
-			<div className="sort__label">
-				<span onClick={() => setIsVisible((prev) => !prev)}>
-					{selected.name}
-				</span>
-			</div>
-			{isVisible && (
-				<div className="sort__popup">
-					<ul>
-						{sortList.map((item, i) => (
-							<li
-								className={
-									selected.sortProperty === item.sortProperty ? "active" : ""
-								}
-								onClick={() => onClickSelected(item)}
-								key={i}
-							>
-								{item.name}
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div ref={sortRef} className="sort">
+      <div className="sort__label">
+        <span onClick={() => setIsVisible((prev) => !prev)}>
+          {selected.name}
+        </span>
+      </div>
+      {isVisible && (
+        <div className="sort__popup">
+          <ul>
+            {sortList.map((item, i) => (
+              <li
+                className={
+                  selected.sortProperty === item.sortProperty ? 'active' : ''
+                }
+                onClick={() => onClickSelected(item)}
+                key={i}
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Sort;
